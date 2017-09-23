8b89c362af2aabd678e69a96197c50c76b7167ed
Keybird0 <keybird0@gmail.com>
2017-09-23 23:42:57 +0800

    add apis in www

--- www/urls.py
+++ www/urls.py
@@ -4,9 +4,12 @@
 __author__ = 'Keybird'
 
 import os, re, time, base64, hashlib, logging
+
 from transwarp.web import get, view, post, ctx, interceptor, seeother, notfound
 
 from models import User, Blog, Comment
+
+from apis import api, APIError, APIValueError, APIPermissionError, APIResourceNotFoundError
 
 @view('blogs.html')
 @get('/')
@@ -14,3 +17,11 @@ def index():
     blogs = Blog.find_all()
     user = User.find_first('where email=?', 'admin@example.com')
     return dict(blogs=blogs, user=user)
+
+@api
+@get('/api/users')
+def api_get_users():
+	users = User.find_by('order by created_at desc')
+	for u in users:
+		u.password = '******'
+	return dict(users=users)
